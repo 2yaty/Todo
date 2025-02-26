8df3eca2ceb95886c22920bd300b17da6d3f3d93
Mohamed Ali Qiaty <eng.ayaty@gmail.com>
2025-02-26 21:51:44 +0200

    feat: added auth controller and service

---
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -0,0 +1,56 @@
+package com.example.demo.service;
+
+
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final TokenService jwtUtil;
+
+    public AuthService(
+            AuthenticationManager authenticationManager,
+            UserDetailsService userDetailsService, PasswordEncoder passwordEncoder,
+            UserRepository userRepository, UserService userService,
+            TokenService jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public AuthResponse authenticate(AuthRequest request) {
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        String token = jwtUtil.generateJwt(auth);
+        return new AuthResponse(token);
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        if (userService.existsByUsername(request.getUsername())) {
+            throw new RuntimeException("Username already taken");
+        }
+
+        User user = new User();
+        user.setLastName(request.getPhone());
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        userService.registerUser(user);
+
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        String token = jwtUtil.generateJwt(auth);
+        return new AuthResponse(token);
+    }
+}
