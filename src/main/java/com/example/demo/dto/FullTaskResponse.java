d6d0f338e6735ca42a7819eb3b1793e9ebecd9d9
Mohamed Ali Qiaty <eng.ayaty@gmail.com>
2025-03-01 06:00:09 +0200

    feat: adding task and workspace DTOs

---
+++ src/main/java/com/example/demo/dto/FullTaskResponse.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class FullTaskResponse {
+    private long id;
+    private String title;
+    private String description;
+}
---
+++ src/main/java/com/example/demo/dto/ShortTaskResponse.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class ShortTaskResponse {
+    private long id;
+    private String title;
+}
