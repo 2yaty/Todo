fda0c00c3da0934b8b5f4f3455829dad4b1c2192
Mohamed Ali Qiaty <eng.ayaty@gmail.com>
2025-03-01 06:31:51 +0200

    fix: adding Status and AllArgsCons

--- src/main/java/com/example/demo/dto/FullTaskResponse.java
+++ src/main/java/com/example/demo/dto/FullTaskResponse.java
@@ -1,10 +1,14 @@
 package com.example.demo.dto;
 
+import com.example.demo.entity.Task;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class FullTaskResponse {
     private long id;
     private String title;
     private String description;
+    private Task.TaskStatus status;
 }
